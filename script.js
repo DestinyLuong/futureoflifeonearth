0558a1e201e80a0389e8cdcdd546ae6cb4a781f3
Destiny Luong <destinyul2010@gmail.com>
2017-04-29 22:49:36 +0000

    Make the questions horizontal, and change background color

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-$(document).ready( function() {
-    $(".start").click( function() {
+var money = 0
+var co2 = 0
+$(document).ready(function() {
+    $("#moneyamount").html(money)
+    $("#co2amount").html(co2)
+    
+    $(".start").click(function() {
+        $("#counter").show()
         $("#intro").hide() 
-       
+        $(".questions").show()
+    })
+    
+    $("#answer2").click(function() {
+        $(".questions").hide()  
+        $("#end").show()
+        money = money + 20
+        co2 = co2 + 20
+        $("#moneyamount").html(money)
+        $("#co2amount").html(co2)
+    })
+    
+    $("#answer1").click(function(){
+        $("#job").hide()
+        money = money + 10
+        co2 = co2 + 10
+        $("#moneyamount").html(money)
+        $("#co2amount").html(co2)
     })
 })
